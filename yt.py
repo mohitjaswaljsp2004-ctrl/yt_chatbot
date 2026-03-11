99f4980bd1152b4895939cbe945e54925b2e71e7
mohitjaswaljsp2004-ctrl <mohitjaswaljsp2004@gmail.com>
2026-03-11 22:36:29 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from yt_dlp import YoutubeDL
 from dotenv import load_dotenv
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_core.prompts import PromptTemplate
@@ -286,3 +286,4 @@ Detailed Answer:
 
             st.success("✅ Answer")
             st.write(answer.content)
+
